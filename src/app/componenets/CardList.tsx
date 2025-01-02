47ab44aab794c6bf1944590a8571f9c53e683f11
EVELO <evelo0702@gmail.com>
2025-01-03 00:03:02 +0900

    update: cardList 랜더링 조건추가

--- src/app/componenets/CardList.tsx
+++ src/app/componenets/CardList.tsx
@@ -6,6 +6,7 @@ import { useMobileSearchStore } from "../store/zustand/globalStore";
 import { useEffect, useState } from "react";
 import { DictData } from "../types/type";
 import Pagination from "./Pagination";
+import Link from "next/link";
 
 export default function CardList() {
   const showMobileSearch = useMobileSearchStore(
@@ -22,7 +23,7 @@ export default function CardList() {
   const [totalPage, setTotalPage] = useState(0);
 
   useEffect(() => {
-    if (data) {
+    if (Array.isArray(data)) {
       setCurrentData(data.slice((currentPage - 1) * 8, currentPage * 8));
       setTotalPage(Math.ceil(data.length / 8));
     }
@@ -35,9 +36,14 @@ export default function CardList() {
     >
       <div className="grid grid-rows-4 grid-cols-2 h-5/6 p-1">
         {currentData?.map((item) => (
-          <div key={item._id} className="border-4 rounded-lg p-4 m-1">
-            {item.title} {/* 데이터 항목 표시 */}
-          </div>
+          <Link
+            href={`/detail/${item._id}`}
+            key={item._id}
+            className="border-4 rounded-lg p-4 m-1 flex flex-col justify-center items-center"
+          >
+            <p>{item.title}</p>
+            <div>{item.category}</div> {/* 데이터 항목 표시 */}
+          </Link>
         ))}
       </div>
 
