94f058943a0338c746c8471b683b0d2eaaa3d6f1
EVELO <evelo0702@gmail.com>
2025-01-03 00:01:53 +0900

    feat: 디테일페이지 , 데이터 캐싱

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -1,12 +1,17 @@
-export default async function Detail({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id } = await params;
-  return (
-    <div>
-      <h1>ComponentName{id}</h1>
-    </div>
-  );
+"use client";
+import { getDictionaryDataById } from "@/app/actions/getDictionaryData.action";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
+
+export default function Detail() {
+  const params = useParams();
+  const id = params.id;
+  const { data } = useQuery({
+    queryKey: ["dictData", id],
+    queryFn: () => getDictionaryDataById(String(id)),
+    staleTime: 1000 * 60 * 5,
+    gcTime: 1000 * 60 * 5,
+  });
+
+  return <div>{data && <h1>{data.title}</h1>}</div>;
 }
