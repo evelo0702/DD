d612ebe6165a2223727b5ba68280424c7060e306
EVELO <evelo0702@gmail.com>
2025-01-06 00:59:44 +0900

    feat: 최근 본 게시물 기능

---
+++ src/app/componenets/RecentBar.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { useRecentDataStore } from "../store/zustand/globalStore";
+import Link from "next/link";
+
+export default function RecentBar() {
+  const { recentData } = useRecentDataStore();
+
+  return (
+    <>
+      {recentData.length > 0 && (
+        <div className="hidden md:block mb-10">
+          <div className="text-center text-2xl">최근 본 게시물</div>
+          <div className="grid grid-cols-1">
+            {recentData.map((i) => (
+              <Link href={`/detail/${i.id}`} key={i.id} className="my-1">
+                <div className="max-w-md mx-auto bg-white border border-gray-300 rounded-lg p-5 shadow-md transform transition-transform hover:scale-105 hover:shadow-lg">
+                  <h3 className="font-bold">{i.title}</h3>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
