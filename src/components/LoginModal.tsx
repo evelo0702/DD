a2782e16f4021c53b02cf728dc18ef8f7322564f
EVELO <evelo0702@gmail.com>
2025-01-09 20:21:46 +0900

    feat: 로그인 모달 생성

---
+++ src/components/LoginModal.tsx
@@ -0,0 +1,100 @@
+"use client";
+import { verifyLogin } from "@/actions/user/verifyLogin.action";
+import { useState } from "react";
+import { useAuthStore } from "@/store/zustand/globalStore";
+import { getUserInfo } from "@/actions/getUserInfo.action";
+
+interface LoginModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const LoginModal: React.FC<LoginModalProps> = ({ isOpen, onClose }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useAuthStore();
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!email || !password) {
+      alert("모든 필드를 입력해주세요.");
+      return;
+    }
+    let res = await verifyLogin(email, password);
+    if (res) {
+      let { token, userInfo } = await getUserInfo();
+      if (token && userInfo) {
+        login(token, userInfo);
+        console.log(`${userInfo.username}님 로그인에 성공하셨습니다`);
+      }
+    }
+
+    setEmail("");
+    setPassword("");
+    onClose();
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+      <div className="bg-white rounded-lg shadow-lg w-full max-w-md p-6 relative">
+        {/* Close Button */}
+        <button
+          onClick={onClose}
+          className="absolute top-4 right-4 text-gray-600 hover:text-gray-800"
+        >
+          &times;
+        </button>
+        {/* Header */}
+        <h2 className="text-xl font-semibold text-gray-800 text-center mb-4">
+          로그인
+        </h2>
+        {/* Form */}
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              이메일
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+              placeholder="example@example.com"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              비밀번호
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+              placeholder="********"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 rounded-md shadow hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+          >
+            로그인
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginModal;
