90430353ecabaa4d92c29eeb050d2cd884d74616
EVELO <evelo0702@gmail.com>
2025-01-11 00:33:27 +0900

    feat: 로그아웃시 페이지 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,15 +6,18 @@ import LoginModal from "./LoginModal";
 import { useAuthStore } from "@/store/zustand/globalStore";
 import { getUserInfo } from "@/actions/user/getUserInfo.action";
 import { logoutAction } from "@/actions/user/logout.action";
+import { useRouter } from "next/navigation";
 
 export default function Header() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const { isAuthenticated, logout, login } = useAuthStore();
+  const { isAuthenticated, logout, login, userData } = useAuthStore();
+  const router = useRouter();
   const handleLogin = () => setIsModalOpen(true);
   const handleLogout = async () => {
     logout();
     let res = await logoutAction();
     console.log(res);
+    router.push("/");
   };
   const persistLogin = async () => {
     let { token, userInfo } = await getUserInfo();
@@ -30,25 +33,38 @@ export default function Header() {
 
   return (
     <header className="flex items-center mx-4">
-      <Image src="/main.png" alt="" width={100} height={100} />
-      <Link className="text-5xl" href="/">
-        DevPedia
-      </Link>
-      <nav className="">
-        <ul>
-          <li>
-            <Link href="/ai" className="mx-4 text-3xl">
-              AI 사전 검색
+      <div className="flex items-center w-1/2">
+        <Image src="/main.png" alt="" width={100} height={100} />
+        <Link className="text-5xl" href="/">
+          DevPedia
+        </Link>
+        <nav className="">
+          <ul>
+            <li>
+              <Link href="/ai" className="mx-4 text-3xl">
+                AI 사전 검색
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="w-1/2 flex justify-end">
+        {isAuthenticated ? (
+          <>
+            <button className="" onClick={handleLogout}>
+              로그아웃
+            </button>
+            <Link href={`/mypage/${userData?.username}`}>마이페이지</Link>
+          </>
+        ) : (
+          <>
+            <Link className="mx-2" href={"/signup"}>
+              회원가입
             </Link>
-            <Link href={"/signup"}>회원가입</Link>
-          </li>
-        </ul>
-      </nav>
-      {isAuthenticated ? (
-        <button onClick={handleLogout}>로그아웃</button>
-      ) : (
-        <button onClick={handleLogin}>로그인!</button>
-      )}
+            <button onClick={handleLogin}>로그인</button>
+          </>
+        )}
+      </div>
       <LoginModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
     </header>
   );
