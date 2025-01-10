349b9f159fa7fa3185e906fff7c758e51d6397f9
EVELO <evelo0702@gmail.com>
2025-01-11 00:23:16 +0900

    del: 불필요한 요소 제거

---
+++ src/components/MyPageLayout.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useState } from "react";
+import Main from "./MyPageMain";
+import { UsersaveData } from "@/types/type";
+import SideBar from "./SideBar";
+
+export default function MyPageLayout({ data }: { data: UsersaveData }) {
+  const [isActive, setIsActive] = useState("user");
+  const [folder, setFolder] = useState(0);
+  let folderName = data.folders.map((folder) => folder.name);
+  console.log(data);
+  return (
+    <>
+      <div className="md:col-span-1 max-[766px]:row-span-1">
+        <SideBar
+          folderName={folderName}
+          setIsActive={setIsActive}
+          isActive={isActive}
+          setFolder={setFolder}
+        />
+      </div>
+      <div className="md:col-span-9 max-[766px]:row-span-6">
+        <Main isActive={isActive} data={data} folder={folder} />
+      </div>
+    </>
+  );
+}
+
+// const [darkMode, setDarkMode] = useState(false); {darkMode && <BsFileEarmarkPerson />}{darkMode && <BiSolidLike />} {darkMode && <FaFolderOpen className="text-white" />}
