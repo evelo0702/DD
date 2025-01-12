979ec43276d8141da2e5245ec3bd33f3202927c9
EVELO <evelo0702@gmail.com>
2025-01-12 17:19:50 +0900

    update: 빌드문제처리

--- src/app/mypage/[username]/page.tsx
+++ src/app/mypage/[username]/page.tsx
@@ -1,24 +1,3 @@
-// import { getUserSaveData } from "@/actions/user/getUserInfo.action";
-// import MyPageLayout from "@/components/MyPageLayout";
-
-// export default async function MyPage({
-//   searchParams,
-// }: {
-//   searchParams: Promise<{ username: string }>;
-// }) {
-//   const params = await searchParams;
-//   console.log(params);
-//   const { username } = params;
-//   console.log(username);
-
-//   const res = await getUserSaveData(decodeURIComponent(username));
-//   console.log(res);
-//   return (
-//     <div className="grid md:grid-cols-10 max-[766px]:grid-rows-7 w-full h-80vh">
-//       {/* {res && <MyPageLayout data={res} />}  */}
-//     </div>
-//   );
-// }
 import { getUserSaveData } from "@/actions/user/getUserInfo.action";
 import MyPageLayout from "@/components/MyPageLayout";
 export default async function MyPage({
@@ -27,7 +6,7 @@ export default async function MyPage({
   params: Promise<{ username: string }>;
 }) {
   const { username } = await params;
-  let res = await getUserSaveData(decodeURIComponent(username));
+  let res = await getUserSaveData(username);
   return (
     <div className="grid md:grid-cols-10 max-[766px]:grid-rows-7 w-full h-80vh">
       <MyPageLayout data={res} />
