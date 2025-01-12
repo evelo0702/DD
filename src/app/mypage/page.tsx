982d96be767f3207b2e570d37b130f19467b6cb5
EVELO <evelo0702@gmail.com>
2025-01-12 16:54:37 +0900

    update: 빌드 오류 해결

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -7,7 +7,7 @@ export default async function MyPage({
   searchParams: Promise<{ username: string }>;
 }) {
   let { username } = await searchParams;
-  const res = await getUserSaveData(decodeURIComponent(username));
+  const res = await getUserSaveData(decodeURIComponent(username!));
 
   return (
     <div className="grid md:grid-cols-10 max-[766px]:grid-rows-7 w-full h-80vh">
