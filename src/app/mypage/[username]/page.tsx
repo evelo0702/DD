ff20640fe9bc4a250acb67245c20006abc73826d
EVELO <evelo0702@gmail.com>
2025-01-12 17:16:38 +0900

    fix: 마이페이지 오류 수정

--- src/app/mypage/[username]/page.tsx
+++ src/app/mypage/[username]/page.tsx
@@ -1,19 +1,36 @@
+// import { getUserSaveData } from "@/actions/user/getUserInfo.action";
+// import MyPageLayout from "@/components/MyPageLayout";
+
+// export default async function MyPage({
+//   searchParams,
+// }: {
+//   searchParams: Promise<{ username: string }>;
+// }) {
+//   const params = await searchParams;
+//   console.log(params);
+//   const { username } = params;
+//   console.log(username);
+
+//   const res = await getUserSaveData(decodeURIComponent(username));
+//   console.log(res);
+//   return (
+//     <div className="grid md:grid-cols-10 max-[766px]:grid-rows-7 w-full h-80vh">
+//       {/* {res && <MyPageLayout data={res} />}  */}
+//     </div>
+//   );
+// }
 import { getUserSaveData } from "@/actions/user/getUserInfo.action";
 import MyPageLayout from "@/components/MyPageLayout";
-
 export default async function MyPage({
-  searchParams,
+  params,
 }: {
-  searchParams: Promise<{ username: string }>;
+  params: Promise<{ username: string }>;
 }) {
-  const params = await searchParams; // searchParams Promise 해제
-  const { username } = params; // username 추출
-
-  const res = await getUserSaveData(decodeURIComponent(username)); // 데이터 가져오기
-
+  const { username } = await params;
+  let res = await getUserSaveData(decodeURIComponent(username));
   return (
     <div className="grid md:grid-cols-10 max-[766px]:grid-rows-7 w-full h-80vh">
-      <MyPageLayout data={res} /> {/* 데이터를 이용해 렌더링 */}
+      <MyPageLayout data={res} />
     </div>
   );
 }
