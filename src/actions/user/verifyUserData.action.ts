a5b9cb78fcb0cbcf10dd064cf0e241b230b2abce
EVELO <evelo0702@gmail.com>
2025-01-13 22:01:15 +0900

    feat: 유저 검증 서버액션 생성

--- src/actions/user/verifyUserData.action.ts
+++ src/actions/user/verifyUserData.action.ts
@@ -1,5 +1,9 @@
 "use server";
 import { connectDB } from "@/utils/database";
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
+import { UserInfo } from "@/types/type";
+
 const db = (await connectDB).db("DevPedia");
 
 export async function verifyEmailAction(email: string): Promise<boolean> {
@@ -19,4 +23,23 @@ export async function verifyUsernameAction(username: string): Promise<boolean> {
     console.error("Error verify username :", error);
     throw new Error("Error verify username");
   }
+}
+
+export async function verifyUserPermission(username: string): Promise<boolean> {
+  const cookieStore = cookies();
+  const token = (await cookieStore).get("token")?.value ?? null;
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET!) as UserInfo;
+      if (decoded.username === username) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      console.error("Failed verify User Permission:", error);
+      return false; // 토큰 검증 실패
+    }
+  }
+  return false;
 }
