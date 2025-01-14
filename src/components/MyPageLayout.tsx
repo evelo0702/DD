f563914fd22f195c5330c32ef7609b1abd50fddf
EVELO <evelo0702@gmail.com>
2025-01-15 07:36:08 +0900

    feat: 데이터 바인딩및 캐싱

--- src/components/MyPageLayout.tsx
+++ src/components/MyPageLayout.tsx
@@ -1,26 +1,53 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Main from "./MyPageMain";
-import { UsersaveData } from "@/types/type";
+import { UserData } from "@/types/type";
 import SideBar from "./SideBar";
+import { useQuery } from "@tanstack/react-query";
+import { getUserData } from "@/actions/user/getUserInfo.action";
 
-export default function MyPageLayout({ data }: { data: UsersaveData }) {
+export default function MyPageLayout({
+  initialdata,
+  username,
+}: {
+  initialdata: UserData;
+  username: string;
+}) {
   const [isActive, setIsActive] = useState("user");
   const [folder, setFolder] = useState(0);
-  let folderName = data.folders.map((folder) => folder.title);
-  console.log(data);
+
+  const { data, refetch } = useQuery({
+    queryKey: ["userData"],
+    queryFn: () => getUserData(username),
+    initialData: initialdata,
+    staleTime: 1000 * 60 * 5,
+    gcTime: 1000 * 60 * 5,
+  });
+  let folderName = data.folders && data.folders.map((folder) => folder.title);
+
   return (
     <>
-      <div className="md:col-span-1 max-[766px]:row-span-1">
+      <div className="md:col-span-2 max-[767px]:row-span-1">
         <SideBar
+          userName={data.username}
           folderName={folderName}
           setIsActive={setIsActive}
+          folder={folder}
           isActive={isActive}
           setFolder={setFolder}
+          refetch={refetch}
         />
       </div>
-      <div className="md:col-span-9 max-[766px]:row-span-6">
-        <Main isActive={isActive} data={data} folder={folder} />
+      <div className="md:h-70vh md:col-span-10 max-[767px]:row-span-11 ">
+        <Main
+          refetch={refetch}
+          isActive={isActive}
+          folderName={folderName}
+          data={data}
+          folder={folder}
+          setIsActive={setIsActive}
+          setFolder={setFolder}
+        />
       </div>
     </>
   );
