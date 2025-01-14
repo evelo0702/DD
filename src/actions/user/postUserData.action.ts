b4ab5d82f42787d89b43e8ce036a19ca13ab8dab
EVELO <evelo0702@gmail.com>
2025-01-15 07:34:40 +0900

    feat: 폴더 추가 , 삭제, 수정 서버액션 추가

--- src/actions/user/postUserData.action.ts
+++ src/actions/user/postUserData.action.ts
@@ -49,3 +49,46 @@ export async function updateUserCategory(
     return { status: 500, msg: "Failed update User Category" };
   }
 }
+interface User {
+  username: string;
+  folders: { title: string; savedPosts: any[] }[];
+}
+export async function updateUserFolders(
+  title: string,
+  username: string,
+  type: string,
+  newTitle?: string 
+) {
+  try {
+    if (type === "add") {
+      await db.collection<User>("users").updateOne(
+        { username }, // username이 일치하는 사용자 찾기
+        {
+          $push: {
+            folders: {
+              title, // 전달받은 title
+              savedPosts: [], // 빈 savedPosts 배열
+            },
+          },
+        }
+      );
+      return { status: 200, msg: "Success Update User Folders" };
+    }
+    if (type === "del") {
+      await db
+        .collection<User>("users")
+        .updateOne({ username }, { $pull: { folders: { title } } });
+    }
+    if (type === "change") {
+      await db
+        .collection<User>("users")
+        .updateOne(
+          { username, "folders.title": title },
+          { $set: { "folders.$.title": newTitle } }
+        );
+    }
+  } catch (err) {
+    console.error(err);
+    return { status: 500, msg: "Failed update User Folders" };
+  }
+}
