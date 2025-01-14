60a19ad643966336cfbb3a95f512a543d72edfe5
EVELO <evelo0702@gmail.com>
2025-01-14 15:39:30 +0900

    feat: 마이페이지 서버액션 및 페이지 권한 검증 서버액션 추가

--- src/app/mypage/[username]/page.tsx
+++ src/app/mypage/[username]/page.tsx
@@ -1,4 +1,5 @@
-import { getUserSaveData } from "@/actions/user/getUserInfo.action";
+import { getUserData } from "@/actions/user/getUserInfo.action";
+import { verifyUserPermission } from "@/actions/user/verifyUserData.action";
 import MyPageLayout from "@/components/MyPageLayout";
 export default async function MyPage({
   params,
@@ -6,10 +7,15 @@ export default async function MyPage({
   params: Promise<{ username: string }>;
 }) {
   const { username } = await params;
-  let res = await getUserSaveData(username);
-  return (
-    <div className="grid md:grid-cols-10 max-[766px]:grid-rows-7 w-full h-80vh">
-      <MyPageLayout data={res} />
-    </div>
-  );
+  let res = await getUserData(username);
+  let correct = await verifyUserPermission(username);
+  if (correct) {
+    return (
+      <div className="grid md:grid-cols-10 max-[766px]:grid-rows-12 w-full">
+        <MyPageLayout initialdata={res} username={username} />
+      </div>
+    );
+  } else {
+    return <div> 해당 페이지에 대한 권한이 없습니다</div>;
+  }
 }
