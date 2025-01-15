b5545ad38fbb43e8f2a6aa25b402d49fd6295acd
EVELO <evelo0702@gmail.com>
2025-01-15 21:51:44 +0900

    feat: 마이페이지 좋아요 저장 삭제기능 추가

--- src/components/MyPageMain.tsx
+++ src/components/MyPageMain.tsx
@@ -22,13 +22,15 @@ export default function Main({
   isActive: string;
   folder: number;
   setFolder: (type: number) => void;
-  folderName: string[];
+  folderName: string[] | "";
   setIsActive: (type: string) => void;
   refetch: () => Promise<QueryObserverResult<UserData>>;
 }) {
   const likeData = data.likedPosts;
   const saveData = data.folders && data.folders[folder];
   const [editMode, setEditMode] = useState(false);
+  const [changeTitle, setChangeTitle] = useState(false);
+  const [newTitle, setNewTitle] = useState("");
   const { data: categoryData } = useQuery<CategoryRes>({
     queryKey: ["category"],
     queryFn: getCategoryData,
@@ -43,11 +45,20 @@ export default function Main({
       setCategory([...editCategory, data]);
     }
   };
-  const handleSaveCategory = async (username: string, data: CategoryData[]) => {
-    let result = await updateUserCategory(data, username);
-    console.log(result);
-    await refetch();
+  const handleSaveCategory = async (
+    username: string,
+    newData: CategoryData[]
+  ) => {
+    console.log(data.saveCategory);
+    console.log(newData);
+    if (data.saveCategory !== newData) {
+      let result = await updateUserCategory(newData, username);
+      console.log(result);
+      await refetch();
+    }
+    return;
   };
+
   useEffect(() => {
     if (data && data.saveCategory) {
       setCategory(data.saveCategory);
@@ -63,36 +74,40 @@ export default function Main({
     if (newTitle.length === 0) {
       return alert("폴더명을 입력해주세요");
     }
-    let res = await updateUserFolders(title, data.username, "change", newTitle);
-    await refetch();
-    console.log(res);
+    if (newTitle !== saveData.title) {
+      let res = await updateUserFolders(
+        title,
+        data.username,
+        "change",
+        newTitle
+      );
+      await refetch();
+      console.log(res);
+    }
   };
-  const [changeTitle, setChangeTitle] = useState(false);
-  const [newTitle, setNewTitle] = useState("");
   return (
-    <div className="w-full h-full  p-7 border-4 border-brown-50 md:rounded-e-lg rounded-b-lg bg-white shadow-lg ">
+    <div className="w-full h-full p-3 border-4 border-brown-50 md:rounded-e-lg rounded-b-lg bg-white shadow-lg flex">
       {isActive === "user" && (
-        <div className="grid gap-6">
-          <div>
-            {editMode ? (
-              <button
-                onClick={() => {
-                  handleSaveCategory(data.username, editCategory);
-                  setEditMode(false);
-                }}
-              >
-                저장하기
-              </button>
-            ) : (
-              <button onClick={() => setEditMode(true)}>수정하기</button>
-            )}
-          </div>
-          {/* 유저 정보 섹션 */}
-          <div className="p-5 bg-gray-50 border rounded-lg shadow-md">
+        <div className="grid gap-2 h-full w-full">
+          <div className="p-5 bg-gray-50 border rounded-lg shadow-md flex flex-col h-full">
+            <div className="text-end">
+              {editMode ? (
+                <button
+                  onClick={() => {
+                    handleSaveCategory(data.username, editCategory);
+                    setEditMode(false);
+                  }}
+                >
+                  저장하기
+                </button>
+              ) : (
+                <button onClick={() => setEditMode(true)}>수정하기</button>
+              )}
+            </div>
             {data && (
-              <div className="space-y-2 text-lg font-medium text-gray-800">
+              <div className="space-y-2 text-lg font-medium text-gray-800 flex-grow">
                 <div className="flex">
-                  <span className="font-semibold text-brown-600">닉네임:</span>{" "}
+                  <span className="font-semibold text-brown-600">닉네임:</span>
                   <p>{data.username}</p>
                 </div>
                 <p>
@@ -144,14 +159,11 @@ export default function Main({
               </div>
             )}
           </div>
-
-          {/* 좋아요 한 글 섹션 */}
-          <div className="p-5 bg-gray-50 border rounded-lg shadow-md">
+          <div className="p-5 bg-gray-50 border rounded-lg shadow-md h-full flex flex-col">
             <div className="flex justify-between items-center mb-4">
-              <h2 className=" text-lg font-semibold text-gray-700">
+              <h2 className="text-lg font-semibold text-gray-700">
                 좋아요 한 글 : {data.likedPosts && data.likedPosts.length}개
               </h2>
-
               <button
                 onClick={() => setIsActive("like")}
                 className="px-4 py-2 text-sm font-medium text-white bg-brown-600 rounded-lg hover:bg-brown-700 transition"
@@ -159,13 +171,13 @@ export default function Main({
                 전체 글 보러가기
               </button>
             </div>
-            <div className="grid grid-cols-3 gap-3">
+            <div className="grid grid-cols-3 gap-3 flex-grow">
               {data.likedPosts &&
                 data.likedPosts.slice(0, 6).map((i, index) => (
                   <Link
                     href={`/detail/${i.id}`}
                     key={index}
-                    className="p-3  rounded-lg shadow hover:shadow-lg transition-all"
+                    className="p-3 rounded-lg shadow hover:shadow-lg transition-all"
                   >
                     <p className="text-sm font-medium text-gray-800">
                       {i.title}
@@ -174,13 +186,11 @@ export default function Main({
                 ))}
             </div>
           </div>
-
-          {/* 폴더 섹션 */}
-          <div className="p-5 bg-gray-50 border rounded-lg shadow-md">
+          <div className="p-5 bg-gray-50 border rounded-lg shadow-md h-full flex flex-col">
             <h2 className="mb-4 text-lg font-semibold text-gray-700">
               저장한 폴더
             </h2>
-            <div className="grid grid-cols-3 gap-4">
+            <div className="grid grid-cols-3 gap-4 flex-grow">
               {data.folders &&
                 data.folders.map((i, index) => (
                   <div
@@ -209,67 +219,75 @@ export default function Main({
         </div>
       )}
       {isActive === "like" && (
-        <div>
-          <div className="p-5 bg-gray-50 border rounded-lg shadow-md">
-            <CardList totaldata={likeData} folder={folder} />
-          </div>
+        <div className="w-full h-full p-5 bg-gray-50 border rounded-lg shadow-md">
+          <CardList
+            totaldata={likeData}
+            folder={folder}
+            isActive={isActive}
+            refetch={refetch}
+          />
         </div>
       )}
       {isActive === "save" && saveData && (
-        <div>
+        <div className="w-full h-full flex flex-col">
           <div className="max-[767px]:block hidden">
             {folderName &&
               folderName.map((i, index) => (
-                <button
-                  key={index}
-                  className=""
-                  onClick={() => setFolder(index)}
-                >
+                <button key={index} onClick={() => setFolder(index)}>
                   {i}
                 </button>
               ))}
           </div>
-          <div className="text-end mb-2">
-            {changeTitle ? (
-              <>
-                <input
-                  type="text"
-                  value={newTitle}
-                  onChange={(e) => setNewTitle(e.target.value)}
-                />
-                <button
-                  className="border rounded-lg p-2 bg-red-600 text-white "
-                  onClick={() => {
-                    setChangeTitle(false);
-                    updateFolderName(saveData.title, newTitle);
-                  }}
-                >
-                  변경 저장
-                </button>
-              </>
-            ) : (
-              <>
-                <div>{folderName[folder]}</div>
-                <button
-                  className="border rounded-lg p-2 bg-red-600 text-white "
-                  onClick={() => {
-                    setChangeTitle(true);
-                  }}
-                >
-                  폴더명 수정하기
-                </button>
-              </>
-            )}
-
-            <button
-              className="border rounded-lg p-2 bg-red-600 text-white "
-              onClick={() => delFolder(saveData.title)}
-            >
-              폴더 삭제하기
-            </button>
+          <div className="mb-2 flex justify-between">
+            <div>{folderName && folderName[folder]}</div>
+            <div>
+              {changeTitle ? (
+                <>
+                  <input
+                    type="text"
+                    value={newTitle}
+                    onChange={(e) => setNewTitle(e.target.value)}
+                  />
+                  <button
+                    className="border rounded-lg p-2 bg-red-600 text-white"
+                    onClick={() => {
+                      setChangeTitle(false);
+                      updateFolderName(saveData.title, newTitle);
+                    }}
+                  >
+                    변경 저장
+                  </button>
+                </>
+              ) : (
+                <>
+                  <button
+                    className="border rounded-lg p-2 bg-red-600 text-white"
+                    onClick={() => {
+                      setChangeTitle(true);
+                    }}
+                  >
+                    폴더명 수정하기
+                  </button>
+                </>
+              )}
+              <button
+                className="border rounded-lg p-2 bg-red-600 text-white"
+                onClick={() => delFolder(saveData.title)}
+              >
+                폴더 삭제하기
+              </button>
+            </div>
           </div>
-          <div className="p-5 bg-gray-50 border rounded-lg shadow-md">
-            <CardList totaldata={saveData.savedPosts} folder={folder} />
+          <div className="flex-grow p-1 bg-gray-50 border rounded-lg shadow-md">
+            {folderName && (
+              <CardList
+                totaldata={saveData.savedPosts}
+                folder={folder}
+                isActive={isActive}
+                title={folderName[folder]}
+                refetch={refetch}
+              />
+            )}
           </div>
         </div>
       )}
