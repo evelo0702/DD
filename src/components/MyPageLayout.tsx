39ff718efb7cf07d0058cc4b8475999557e80480
EVELO <evelo0702@gmail.com>
2025-01-15 21:51:19 +0900

    update: 레이아웃 조건 변경

--- src/components/MyPageLayout.tsx
+++ src/components/MyPageLayout.tsx
@@ -23,7 +23,8 @@ export default function MyPageLayout({
     staleTime: 1000 * 60 * 5,
     gcTime: 1000 * 60 * 5,
   });
-  let folderName = data.folders && data.folders.map((folder) => folder.title);
+  let folderName =
+    data.folders && data.folders.map((folder) => folder.title || "");
 
   return (
     <>
