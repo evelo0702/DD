6185b6ed7fdc8abc1af5462d450f298938b37e0f
EVELO <evelo0702@gmail.com>
2025-01-19 00:44:48 +0900

    update: 마이페이지 검증 추가

--- src/app/mypage/[username]/page.tsx
+++ src/app/mypage/[username]/page.tsx
@@ -10,13 +10,13 @@ export default async function MyPage({
   let correct = await verifyUserPermission(username);
   let res = await getUserData(username);
 
-  if (correct) {
+  if (correct && res && res.username === decodeURIComponent(username)) {
     return (
       <div className="grid md:grid-cols-12 max-[766px]:grid-rows-12 w-full ">
         <MyPageLayout initialdata={res} username={username} />
       </div>
     );
   } else {
-    return <div> 해당 페이지에 대한 권한이 없습니다</div>;
+    return <div>해당 페이지에 대한 권한이 없습니다 로그인을 다시 해주세요</div>;
   }
 }
