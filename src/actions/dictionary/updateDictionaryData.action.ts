4609f75ed29245cfbe41d581c7b73a135d28ed54
EVELO <evelo0702@gmail.com>
2025-01-19 00:41:56 +0900

    feat: 좋아요 저장 취소기능 액션 추가

--- src/actions/dictionary/updateDictionaryData.action.ts
+++ src/actions/dictionary/updateDictionaryData.action.ts
@@ -4,24 +4,30 @@ import { connectDB } from "@/utils/database";
 import { ObjectId } from "mongodb";
 
 const db = (await connectDB).db("DevPedia");
-export async function addLikeNumber(id: string) {
+export async function editLikeNumber(id: string, type: string) {
   try {
     const objectId = new ObjectId(id);
     await db
       .collection("dictionary")
-      .updateOne({ _id: objectId }, { $inc: { like: 1 } });
+      .updateOne(
+        { _id: objectId },
+        { $inc: { like: type === "add" ? 1 : -1 } }
+      );
     return { status: 200, msg: "Success add number" };
   } catch (err) {
     console.error(err);
     return { status: 500, msg: "Failed add number" };
   }
 }
-export async function addSaveNumber(id: string) {
+export async function editSaveNumber(id: string, type: string) {
   try {
     const objectId = new ObjectId(id);
     await db
       .collection("dictionary")
-      .updateOne({ _id: objectId }, { $inc: { save: 1 } });
+      .updateOne(
+        { _id: objectId },
+        { $inc: { save: type === "add" ? 1 : -1 } }
+      );
     return { status: 200, msg: "Success add number" };
   } catch (err) {
     console.error(err);
