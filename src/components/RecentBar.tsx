73f759cc251612b917b33021d1ebd49473883c23
EVELO <evelo0702@gmail.com>
2025-01-20 15:01:03 +0900

    update: 최근게시물 디자인 변경

--- src/components/RecentBar.tsx
+++ src/components/RecentBar.tsx
@@ -10,12 +10,14 @@ export default function RecentBar() {
     <>
       {pathname !== "/signup" && recentData.length > 0 && (
         <div className="hidden md:block mb-10 me-4">
-          <div className="text-center text-2xl">최근 본 게시물</div>
+          <div className="text-center text-2xl font-semibold">
+            최근 본 게시물
+          </div>
           <div className="grid grid-cols-1">
             {recentData.map((i) => (
               <Link href={`/detail/${i.id}`} key={i.id} className="my-1">
                 <div className="mx-auto bg-white border border-gray-300 rounded-lg p-5 shadow-md transform transition-transform hover:scale-105 hover:shadow-lg">
-                  <h3 className="font-bold">{i.title}</h3>
+                  <h3 className="text-lg text-center">{i.title}</h3>
                 </div>
               </Link>
             ))}
