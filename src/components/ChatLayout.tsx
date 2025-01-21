3cf4de3ca5feb272ffa33d37fb8a8841d993a31e
EVELO <evelo0702@gmail.com>
2025-01-21 22:01:16 +0900

    feat: 채팅레이아웃 css변경

--- src/components/ChatLayout.tsx
+++ src/components/ChatLayout.tsx
@@ -65,10 +65,10 @@ export default function ChatLayout() {
   }, [messages]);
   return (
     <div className="flex flex-col h-full bg-gray-100 p-5">
-      <div className="flex flex-col flex-grow max-w-3xl mx-auto bg-white shadow-lg rounded-lg  w-full h-full p-5">
+      <div className="flex flex-col flex-grow max-w-3xl mx-auto bg-white shadow-lg rounded-lg w-full h-full p-5">
         {/* 메시지 목록 */}
         <div className="h-full overflow-y-auto">
-          <div className="p-4  grid max-[640px]:grid-cols-2 sm:grid-cols-4 max-[400px]:grid-cols-1 gap-2">
+          <div className="p-4 grid max-[640px]:grid-cols-2 sm:grid-cols-4 max-[400px]:grid-cols-1 gap-2">
             {messages.map((msg, index) =>
               msg.type === "msg" ? (
                 <div
@@ -76,7 +76,7 @@ export default function ChatLayout() {
                   className={`col-span-4 max-[640px]:col-span-2 max-[400px]:col-span-1 mb-2 p-3 rounded-lg text-xl ${
                     msg.sender === "bot"
                       ? "bg-gray-200 text-gray-800"
-                      : "bg-blue-200  text-end"
+                      : "bg-blue-200 text-end"
                   }`}
                 >
                   {msg.text}
@@ -89,7 +89,7 @@ export default function ChatLayout() {
                 >
                   <div className="group max-w-xs bg-white border-2 border-gray-200 rounded-lg overflow-hidden transform transition-all duration-300 hover:scale-105 hover:shadow-2xl hover:border-blue-300">
                     <div className="p-6 max-[400px]:p-1 max-[400px]:text-center">
-                      <h2 className="text-2xl  text-gray-900  transition-colors duration-300">
+                      <h2 className="text-2xl text-gray-900 transition-colors duration-300">
                         {msg.text}
                       </h2>
                     </div>
@@ -105,8 +105,9 @@ export default function ChatLayout() {
             <div ref={messageEndRef} />
           </div>
         </div>
+
         {/* 입력창 */}
-        <div className="w-full flex items-center p-4 border-t border-gray-200">
+        <div className="flex-shrink-0 w-full flex items-center p-4 border-t border-gray-200">
           <input
             type="text"
             className="flex-grow p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 text-xl"
@@ -115,7 +116,7 @@ export default function ChatLayout() {
             onChange={(e) => setInput(e.target.value)}
           />
           <button
-            className="ml-4 px-4 py-2 bg-blue-200 hover:bg-slate-600 hover:text-white rounded-lg  text-xl"
+            className="ml-4 px-4 py-2 bg-blue-200 hover:bg-slate-600 hover:text-white rounded-lg text-xl"
             onClick={sendMessage}
           >
             Send
