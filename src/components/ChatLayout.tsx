2f77785c45a67d2e9d2f90c391791ce8a79a992d
EVELO <evelo0702@gmail.com>
2025-01-21 22:07:26 +0900

    update: 채팅 레이아웃 css 변경

--- src/components/ChatLayout.tsx
+++ src/components/ChatLayout.tsx
@@ -65,7 +65,7 @@ export default function ChatLayout() {
   }, [messages]);
   return (
     <div className="flex flex-col h-full bg-gray-100 p-5">
-      <div className="flex flex-col flex-grow max-w-3xl mx-auto bg-white shadow-lg rounded-lg w-full h-full p-5">
+      <div className="flex flex-col max-w-3xl mx-auto bg-white shadow-lg rounded-lg w-full h-full p-5">
         {/* 메시지 목록 */}
         <div className="h-full overflow-y-auto">
           <div className="p-4 grid max-[640px]:grid-cols-2 sm:grid-cols-4 max-[400px]:grid-cols-1 gap-2">
@@ -107,16 +107,16 @@ export default function ChatLayout() {
         </div>
 
         {/* 입력창 */}
-        <div className="flex-shrink-0 w-full flex items-center p-4 border-t border-gray-200">
+        <div className="w-full pt-2 border-t border-gray-200 grid grid-cols-10">
           <input
             type="text"
-            className="flex-grow p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 text-xl"
+            className="col-span-9 me-2 max-[400px]:col-span-7 p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 text-xl"
             placeholder="Type your message..."
             value={input}
             onChange={(e) => setInput(e.target.value)}
           />
           <button
-            className="ml-4 px-4 py-2 bg-blue-200 hover:bg-slate-600 hover:text-white rounded-lg text-xl"
+            className="col-span-1 max-[400px]:col-span-3 px-4 py-2 bg-blue-200 hover:bg-slate-600 hover:text-white rounded-lg text-xl"
             onClick={sendMessage}
           >
             Send
