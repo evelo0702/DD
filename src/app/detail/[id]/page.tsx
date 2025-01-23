dc5e1d4cc8418556c8eb237ba34efe6955a715b1
EVELO <evelo0702@gmail.com>
2025-01-23 22:28:48 +0900

    update: 컴포넌트 분리 및 메타 데이터 추가

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -1,170 +1,32 @@
-"use client";
 import { getDictionaryDataById } from "@/actions/dictionary/getDictionaryData.action";
-import {
-  editLikeNumber,
-  editSaveNumber,
-} from "@/actions/dictionary/updateDictionaryData.action";
-import { getUserData } from "@/actions/user/getUserInfo.action";
-import {
-  editLikePost,
-  editSavePost,
-} from "@/actions/user/postUserData.action";
-import CodeBlock from "@/components/CodeBlock";
-import SaveModal from "@/components/SaveModal";
-import { useAuthStore, useRecentDataStore } from "@/store/zustand/globalStore";
-import { DictData, UserData } from "@/types/type";
-import { useQuery } from "@tanstack/react-query";
-import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import { FaRegHeart } from "react-icons/fa";
-import { FcLike } from "react-icons/fc";
-import { MdOutlineBookmarkAdd } from "react-icons/md";
-
-export default function Detail() {
-  const { addData } = useRecentDataStore();
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const { userData } = useAuthStore();
-  const params = useParams();
-  const id = params.id as string;
-  const { data, refetch } = useQuery<DictData>({
-    queryKey: ["dictData", id],
-    queryFn: () => getDictionaryDataById(String(id)),
-    staleTime: 1000 * 60 * 5,
-    gcTime: 1000 * 60 * 5,
-  });
-  const { data: userSaveData, refetch: userRefetch } = useQuery<UserData>({
-    queryKey: ["userData"],
-    queryFn: () => getUserData(userData!.username),
-  });
-  useEffect(() => {
-    if (data) {
-      addData({ id: data._id, title: data.title });
-    }
-  }, [data]);
-  const SaveBtn = async (email: string, folderTitle: string, type: string) => {
-    if (data) {
-      let Postdata = {
-        id: data._id,
-        title: data.title,
-        author: data.author,
-        category: data.category,
-        createdAt: data.createdAt,
-      };
-      let res = await editSaveNumber(id, type);
-      let res2 = await editSavePost(email, folderTitle, Postdata, type);
-      console.log(res);
-      console.log(res2);
-      await refetch();
-      await userRefetch();
-    }
+import DetailPage from "@/components/DetailPage";
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const data = await getDictionaryDataById(id);
+  return {
+    title: `${data.title} 상세정보`,
+    description: `DevPedia에 저장된 ${data.title}의 상세 정보 페이지 입니다`,
+    openGraph: {
+      title: `${data.title} 상세정보`,
+      description: `DevPedia에 저장된 ${data.title}의 상세 정보 페이지 입니다`,
+      images: ["/main.png"],
+    },
   };
-  const LikeBtn = async (email: string, type: string) => {
-    if (data) {
-      let Postdata = {
-        id: data._id,
-        title: data.title,
-        author: data.author,
-        category: data.category,
-        createdAt: data.createdAt,
-      };
-      let res = editLikeNumber(id, type);
-      let res2 = editLikePost(email, Postdata, type);
-      console.log(res);
-      console.log(res2);
-      await refetch();
-      await userRefetch();
-    }
-  };
-
+}
+export default async function PageName({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const data = await getDictionaryDataById(id);
   return (
-    <div className="p-6  rounded-lg shadow-lg h-full max-w-screen-xl">
-      {data && (
-        <div className="grid grid-rows-8 h-full grid-cols-1">
-          <div className="row-span-7 p-2">
-            <div className="mb-4">
-              <div>
-                <p className="text-4xl">제목 : {data.title}</p>
-                <div className="flex justify-end">
-                  <div className="me-4 flex justify-center items-center">
-                    {userData && (
-                      <button
-                        onClick={() =>
-                          LikeBtn(
-                            userData.email,
-                            userSaveData?.likedPosts.some(
-                              (i) => i.id === data._id
-                            )
-                              ? "del"
-                              : "add"
-                          )
-                        }
-                        className="text-2xl me-4"
-                      >
-                        {userSaveData?.likedPosts.some(
-                          (i) => i.id === data._id
-                        ) ? (
-                          <FcLike />
-                        ) : (
-                          <FaRegHeart />
-                        )}
-                      </button>
-                    )}
-
-                    <p className="me-4">좋아요: {Number(data.like)}</p>
-                  </div>
-                  <div className="me-4 flex justify-center items-center">
-                    {userData && (
-                      <button
-                        className="me-4 text-3xl"
-                        onClick={() => setIsModalOpen(true)}
-                      >
-                        <MdOutlineBookmarkAdd />
-                      </button>
-                    )}
-                    <p className="me-4">저장: {data.save}</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="flex max-[550px]:flex-col text-xl justify-end">
-              <div>
-                <span className=" text-gray-600 me-4">
-                  작성자 : {data.author}
-                </span>
-                <span className=" text-gray-600">
-                  작성일 : {data.createdAt}
-                </span>
-              </div>
-            </div>
-            <div className="flex space-x-4 mb-4 items-center">
-              <span className="font-semibold">카테고리 :</span>
-              {data.category.map((i) => (
-                <span
-                  key={i._id}
-                  className="me-2 border rounded-lg p-2 text-lg bg-gray-100 text-gray-800"
-                >
-                  {i.title}
-                </span>
-              ))}
-            </div>
-            <div className="text-2xl text-gray-800 mb-4">{data.content}</div>
-            <div className="overflow-x-auto">
-              <CodeBlock code={data.code} />
-            </div>
-          </div>
-        </div>
-      )}
-      {userSaveData && data && userData && (
-        <SaveModal
-          isOpen={isModalOpen}
-          onClose={() => setIsModalOpen(false)}
-          data={userSaveData}
-          SaveBtn={SaveBtn}
-          id={data._id}
-          username={userData.username}
-          userRefetch={userRefetch}
-        />
-      )}
+    <div>
+      <DetailPage initialData={data} id={id} />
     </div>
   );
 }
